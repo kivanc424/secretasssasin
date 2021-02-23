177eeb8589bd1399d3991d99bd6846023c03b8b6
kivancozmen <kivanc_wow424@live.se>
2021-02-23 17:10:25 +0100

    jj

--- src/Router.js
+++ src/Router.js
@@ -6,6 +6,7 @@ import AllGameRoomView from "./views/AllGameRoomView";
 import CreateRoomView from "./views/CreateRoomView";
 import GameLobby from "./views/GameLobby";
 import ExampleLogin from "./views/ExampleLogin";
+import GameComponent from "./components/GameComponent";
 
 export default function Router() {
   return (
@@ -15,8 +16,9 @@ export default function Router() {
         <Route path="/login" component={Login} />
         <Route path="/create-room" component={CreateRoomView} />
         <Route path="/all-game-room" component={AllGameRoomView} />
-        <Route path="/design" component={ExampleLogin}/>
+        <Route path="/design" component={ExampleLogin} />
         <Route path="/lobby/:id" component={GameLobby} />
+        <Route path="/game" component={GameComponent} />
       </Switch>
     </BrowserRouter>
   );
--- src/components/GameComponent.js
+++ src/components/GameComponent.js
@@ -1,7 +1,19 @@
-import React from 'react'
-
-function GameComponent(props) {
-
+import { React, Component } from "react";
+import styled from "styled-components";
+class GameComponent extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Container>
+          <h1>Hello world</h1>
+        </Container>
+      </Wrapper>
+    );
+  }
 }
 
-export default GameComponent
+export default GameComponent;
+
+const Wrapper = styled.div``;
+
+const Container = styled.div``;
